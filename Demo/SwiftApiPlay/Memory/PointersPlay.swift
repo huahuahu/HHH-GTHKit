60d4d4df25c5de0cff5eda9f23de8b8311668eea
tiger <guotenghu@gmail.com>
2019-03-29 01:12:58 +0800

    深入swift 内部

---
+++ Demo/SwiftApiPlay/Memory/PointersPlay.swift
@@ -0,0 +1,163 @@
+//
+//  PointersPlay.swift
+//  SwiftApiPlay
+//
+//  Created by huahuahu on 2019/3/26.
+//  Copyright © 2019 huahuahu. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Quick
+import Nimble
+import HHHKit
+
+class PointerPlay: QuickSpec {
+    //swiftdisable:function_body_length
+    override func spec() {
+        describe("访问指针内存的方式") {
+            let intNumber = 23
+            let ptr: UnsafeMutablePointer<Int> = UnsafeMutablePointer.allocate(capacity: 1)
+            ptr.initialize(to: intNumber)
+            expect(ptr.pointee).to(equal(23))
+            expect(ptr[0]).to(equal(23))
+        }
+        describe("以另一种类型访问内存") {
+            //type-safe way to temporarily change the bound type of the memory
+            it("可以临时改变Type", closure: {
+                let intArray: [UInt8] = [0x80, 0x81, 0xa1, 0xff, 0x78, 0x10, 0x01, 0x08]
+                let uint8Pointer: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer.allocate(capacity: 8)
+                uint8Pointer.initialize(from: intArray, count: 8)
+                expect(uint8Pointer[0]).to(equal(0x80))
+                expect(uint8Pointer[1]).to(equal(0x81))
+                uint8Pointer.withMemoryRebound(to: Int8.self, capacity: 8, { (intPointer) in
+                    expect(intPointer[0]).to(equal(-128))
+                    expect(intPointer[1]).to(equal(-127))
+                    expect(intPointer[7]).to(equal(8))
+                })
+            })
+            //type-safe way to permanently change the bound type of the memory
+            it("也可以永久访问Type", closure: {
+                let intArray: [UInt8] = [0x80, 0x81, 0xa1, 0xff, 0x78, 0x10, 0x01, 0x08]
+                let uint8Pointer: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer.allocate(capacity: 8)
+                uint8Pointer.initialize(from: intArray, count: 8)
+                expect(uint8Pointer[0]).to(equal(0x80))
+                expect(uint8Pointer[1]).to(equal(0x81))
+                //永久改为UInt64类型
+                let uint64Pointer = UnsafeRawPointer(uint8Pointer)
+                    .bindMemory(to: UInt64.self, capacity: 1)
+                // 小端序存储
+                expect(uint64Pointer.pointee).to(equal(0x08011078_ffa18180))
+                //⚠️⚠️⚠️⚠️⚠️
+                //                var firstByte = uint8Pointer.pointee             // undefined
+            })
+            // load typed instances directly from raw memory
+            it("untyped memory access", closure: {
+                let intArray: [UInt8] = [0x80, 0x81, 0xa1, 0xff, 0x78, 0x10, 0x01, 0x08]
+                let uint8Pointer: UnsafeMutablePointer<UInt8> = UnsafeMutablePointer.allocate(capacity: 8)
+                uint8Pointer.initialize(from: intArray, count: 8)
+                let rawPointer = UnsafeRawPointer(uint8Pointer)
+                let fullInteger = rawPointer.load(as: UInt64.self)   // OK
+                let firstByte = rawPointer.load(as: UInt8.self)      // OK
+                expect(fullInteger).to(equal(0x08011078_ffa18180))
+                expect(firstByte).to(equal(128))
+            })
+        }
+        describe("typedPointer") {
+            it("number to pointers", closure: {
+                var aNumber = 10
+                withUnsafePointer(to: aNumber, { (pointer)  in
+                    expect(pointer.pointee).to(equal(10))
+                    //Left side of mutating operator isn't mutable: 'pointee' is a get-only property
+                    //pointer.pointee += 1
+                    //不能调用这个，pointer being freed was not allocated
+                    //pointer.deallocate()
+                    //⚠️ 这个值是未定义的！！
+                    let succ = pointer.successor()
+                    print("get random number \(succ.pointee)")
+                    //                pointer.pointee += 1
+                })
+                withUnsafeMutablePointer(to: &aNumber, { (pointer) in
+                    pointer.pointee += 1
+                    expect(pointer.pointee).to(equal(11))
+                })
+                expect(aNumber).to(equal(11))
+                withUnsafePointer(to: aNumber, { (pointer) in
+                    pointer.withMemoryRebound(to: UInt8.self, capacity: 8, { (uint8Pointer)  in
+                        let data = Data.init(bytes: uint8Pointer, count: 8)
+                        let strMemory = data.hexView
+                        expect(strMemory).to(equal("0b_00_00_00_00_00_00_00"))
+                        print(strMemory)
+                    })
+                })
+            })
+
+            describe("pointer to class", closure: {
+                let pointer: UnsafeMutablePointer<PointerTestClass> = UnsafeMutablePointer.allocate(capacity: 3)
+                let testInstance = PointerTestClass()
+                pointer.initialize(repeating: testInstance, count: 3)
+                // ⚠️ 下面这个会报错，因为还处于 uninit 状态
+//                pointer.assign(repeating: testInstance, count: 3)
+                testInstance.intNum = 4
+                //改了一个，所有的都会改，因为指向了同一个地址。
+                expect(pointer.pointee.intNum).to(equal(4))
+                expect(pointer[1].intNum).to(equal(4))
+                expect(pointer[2].intNum).to(equal(4))
+                // 证明是同一个地址
+                var instanceAddress: String!
+                withUnsafeBytes(of: testInstance, { (rawBuffer) in
+                    let data = Data.init(bytes: rawBuffer.baseAddress!, count: rawBuffer.count)
+                    instanceAddress = data.hexView
+                })
+                (0..<3).forEach({ (offset) in
+                    pointer.advanced(by: offset).withMemoryRebound(to: UInt8.self, capacity: 8, { (uInt8Pointer) in
+                        let data = Data.init(bytes: uInt8Pointer, count: 8)
+                        expect(data.hexView).to(equal(instanceAddress))
+                    })
+                })
+            })
+            describe("pointer to struct", {
+                let pointer: UnsafeMutablePointer<PointerTestStruct> = UnsafeMutablePointer.allocate(capacity: 3)
+                var testInstance = PointerTestStruct()
+                //可以用assign，因为是 trival type
+                pointer.assign(repeating: testInstance, count: 3)
+                testInstance.intNum = 4
+                //改了一个，其他的不会受影响，因为是不同的实例。
+                expect(pointer.pointee.intNum).to(equal(3))
+                expect(pointer[1].intNum).to(equal(3))
+                expect(pointer[2].intNum).to(equal(3))
+
+                var memory: String!
+                let stride = MemoryLayout.stride(ofValue: testInstance)
+                withUnsafeBytes(of: testInstance, { (rawBuffer) in
+                    let data = Data.init(bytes: rawBuffer.baseAddress!, count: rawBuffer.count)
+                    memory = data.hexView
+                })
+                (0..<3).forEach({ (offset) in
+                    pointer.advanced(by: offset).withMemoryRebound(to: UInt8.self, capacity: stride, { (uInt8Pointer) in
+                        let data = Data.init(bytes: uInt8Pointer, count: stride)
+                        expect(data.hexView).toNot(equal(memory))
+                    })
+                })
+            })
+        }
+    }
+}
+
+struct PointerTestStruct {
+    var intNum = 3
+    var another = 56
+    var another1 = 59
+}
+
+class PointerTestClass {
+    var intNum = 3
+    var another = 56
+    var another1 = 59
+}
+
+extension Data {
+    var hexView: String {
+        return map({String.init(format: "%02x", $0)}).joined(separator: "_")
+    }
+}
---
+++ Demo/SwiftApiPlay/Memory/StringMemoryPlay.swift
@@ -0,0 +1,206 @@
+//
+//  File.swift
+//  SwiftApiPlay
+//
+//  Created by huahuahu on 2019/3/28.
+//  Copyright © 2019 huahuahu. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Quick
+import Nimble
+import HHHKit
+
+class StringMemoryPlay: QuickSpec {
+    //swiftlint:disable function_body_length
+    override func spec() {
+        describe("small string") {
+            func checkLength(_ smallstr: String) {
+                let utf8Count: UInt8 = UInt8(smallstr.utf8.count)
+                withUnsafeBytes(of: smallstr, { (bufferBytePointer) in
+                    // 小字符串只有16字节
+                    expect(bufferBytePointer.count).to(be(16))
+                    let count: UInt8 = bufferBytePointer.last! & 0x0F
+                    let zero: UInt8 = 0
+                    expect(count - utf8Count).to(be(zero))
+                    print("\(smallstr), utf8Length is \(count)")
+                })
+            }
+            func checkAscii(_ smallStr: String) {
+                withUnsafeBytes(of: smallStr, { (bufferBytePointer) in
+                    // 小字符串只有16字节
+                    expect(bufferBytePointer.count).to(be(16))
+                    let discriminator = bufferBytePointer.last! & 0xF0
+                    let isAscii = !smallStr.unicodeScalars.contains { !$0.isASCII }
+                    print("\(smallStr), isAscii? \(isAscii)")
+                    if isAscii {
+                        let asciiDis: UInt8 = 0xe0
+                        expect(discriminator).to(be(asciiDis))
+                    } else {
+                        let noAsciiDis: UInt8 = 0xa0
+                        expect(discriminator).to(be(noAsciiDis))
+                    }
+                })
+            }
+            func checkCodePoints(_ smallStr: String) {
+                withUnsafeBytes(of: smallStr, { (bufferBytePointer) in
+                    // 小字符串只有16字节
+                    expect(bufferBytePointer.count).to(be(16))
+                    print("utf8 points: \(smallStr.utf8.map({String.init(format: "%02x", $0)}).joined(separator: "_"))")
+                    smallStr.utf8.enumerated().forEach({ (offset, point) in
+                        expect(point).to(equal(bufferBytePointer[offset]))
+                    })
+                    let rawBytes = bufferBytePointer.map({String.init(format: "%02x", $0)}).joined(separator: "_")
+                    print("raw bytes:   \(rawBytes)")
+                })
+            }
+            func check(_ smallStr: String) {
+//                print("-------------smallStr------------------------")
+//                checkLength(smallStr)
+//                checkAscii(smallStr)
+//                checkCodePoints(smallStr)
+            }
+            it("表示ascii", closure: {
+                let smallstr = "123"
+                check(smallstr)
+            })
+            it("最长15个", closure: {
+                let smallstr = "1234567890abcde"
+                check(smallstr)
+            })
+            it("表示非ascii", closure: {
+                let smallstr = "iIIïœ"
+                check(smallstr)
+            })
+            it("表示emoji", closure: {
+                //31
+                //F0 9F 90 AF
+                //31
+                //31 E2 83 A3 EF B8 8F
+                //31
+                let smallstr = "1🐯11⃣️1"
+                check(smallstr)
+            })
+        }
+
+        describe("large string") {
+            func checkIsAscii(_ largeStr: String) {
+                withUnsafeBytes(of: largeStr, { (bytesPointer) in
+                    let flags = (bytesPointer.first! & 0xf0) >> 4
+                    let flagIsAscii = flags & 0x80
+                    let isAscii = !largeStr.unicodeScalars.contains { !$0.isASCII }
+                    print("is Ascii: \(isAscii)")
+                    expect(flagIsAscii != 0).to(equal(isAscii))
+                })
+            }
+
+            func checkTailAlloc(_ largeStr: String) {
+                withUnsafeBytes(of: largeStr, { (bytesPointer) in
+                    let flags = (bytesPointer.first! & 0xf0) >> 4
+                    let isTailAllocate = flags & 0x1
+                    expect(isTailAllocate).to(equal(1))
+                })
+            }
+
+            func checkCount(_ largeStr: String) {
+                let utf8Count = largeStr.utf8.count
+                withUnsafeBytes(of: largeStr, { (bytesPointer) in
+                    let count = bytesPointer.baseAddress!.load(as: UInt64.self) & 0x00ffffff_ffffffff
+                    expect(count).to(be(utf8Count))
+                    print("utftCount is \(count)")
+                })
+            }
+            func checkDiscriminator(_ largeStr: inout String) {
+                withUnsafeBytes(of: largeStr, { (bytesPointer) in
+                    let lastInt = bytesPointer.baseAddress!.load(fromByteOffset: 15, as: UInt8.self)
+                    let discriminator = lastInt & 0xf0
+                    let zero: UInt8 = 0
+                    expect(discriminator - 0x80).to(be(zero))
+                })
+            }
+            func checkStringObject(_ largeStr: String) {
+                let address = withUnsafeBytes(of: largeStr, { (bytesPointer) -> UInt64 in
+                    return bytesPointer.baseAddress!.load(fromByteOffset: 8, as: UInt64.self) & 0x0fffffff_ffffffff
+                })
+                print("address: \(address.toStringView)")
+                let rawPointer = UnsafeRawPointer(bitPattern: UInt(address))!
+                let metadata = rawPointer.load(fromByteOffset: 0, as: UInt64.self).toStringView
+                print("metadata: \(metadata)")
+                let refCounts = rawPointer.load(fromByteOffset: 8, as: UInt64.self).toStringView
+                print("refCounts: \(refCounts)")
+                let capAndFlags = rawPointer.load(fromByteOffset: 16, as: UInt64.self).toStringView
+                print("capAndFlags: \(capAndFlags)")
+                let countAndFlags = rawPointer.load(fromByteOffset: 24, as: UInt64.self).toStringView
+                print("countAndFlags: \(countAndFlags)")
+
+                let utfPoints = rawPointer.advanced(by: 32).bindMemory(to: UInt8.self, capacity: largeStr.utf8.count)
+                print("utf8 inMemory:")
+                largeStr.utf8.enumerated().forEach(({ (offset, point) in
+                    print(String.init(format: "%02x", utfPoints.advanced(by: offset).pointee), separator: "_", terminator: "_")
+                    expect(utfPoints.advanced(by: offset).pointee).to(equal(point))
+                }))
+            }
+//            func checkDiscriminatorImmortal(_ largeStr: String) {
+//                withUnsafeBytes(of: largeStr, { (bytesPointer) in
+//                    let secondInt = bytesPointer.baseAddress!.load(fromByteOffset: 8, as: UInt64.self)
+//                    let discriminator = secondInt & 0xf0
+//                    expect(discriminator).to(be(0))
+//                })
+//
+//            }
+
+            func check(_ largeStr: inout String) {
+                print("----large:== \(largeStr) ==------------")
+                print("utf8 points:\n\(largeStr.utf8.map({String.init(format: "%02x", $0)}).joined(separator: "_"))")
+                let size = MemoryLayout.size(ofValue: largeStr)
+                expect(size).to(be(16))
+                let stride = MemoryLayout.stride(ofValue: largeStr)
+                expect(stride).to(be(16))
+//                checkIsAscii(largeStr)
+                checkCount(largeStr)
+//                checkTailAlloc(largeStr)
+                checkDiscriminator(&largeStr)
+                checkStringObject(largeStr)
+            }
+//            it("检测可变形", closure: {
+//                let largeStr = "1🐯11⃣️1🇺🇸1"
+//                checkDiscriminatorImmortal(largeStr)
+//                var largeStr1 = "1🐯11⃣️1🇺🇸1"
+//                checkDiscriminator(&largeStr1)
+//                largeStr1 += "hha"
+//                print(largeStr1)
+//            })
+            it("表示emoji", closure: {
+                //31
+                //F0 9F 90 AF
+                //31
+                //31 E2 83 A3 EF B8 8F
+                //31
+                //F0 9F 87 BA F0 9F 87 B8
+                //31
+                var largeStr = "1🐯11⃣️1🇺🇸1"
+                check(&largeStr)
+                print("\n")
+                largeStr += "hha"
+                print(largeStr)
+            })
+            it("表示中文", closure: {
+                var largeStr = "我喜欢你，你知不知道"
+                check(&largeStr)
+                print("\n")
+                largeStr += "hha"
+                print(largeStr)
+            })
+        }
+    }
+}
+
+extension UInt64 {
+    var toStringView: String {
+        let str = String.init(self, radix: 16, uppercase: false)
+        let count = str.count
+        return "0x" + String.init(repeating: "0", count: (16 - count)) + str
+    }
+
+}
